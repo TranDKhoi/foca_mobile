72ec93e1b1fd2aed3b9fc858a268cb381246db29
TranDKhoi <tranxyz88@gmail.com>
2022-04-30 21:35:51 +0700

    .
    .

--- app/src/main/java/com/example/foca_mobile/activity/MainActivity.kt
+++ app/src/main/java/com/example/foca_mobile/activity/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.foca_mobile.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -12,6 +10,7 @@ import com.example.foca_mobile.activity.admin.home.AdminHomeFragment
 import com.example.foca_mobile.activity.authen.login.LoginScreen
 import com.example.foca_mobile.activity.user.chat.listmess.ListMessageFragment
 import com.example.foca_mobile.activity.user.home.HomeFragment
+import com.example.foca_mobile.activity.user.home.UserHomeFragment
 import com.example.foca_mobile.databinding.ActivityMainBinding
 import com.example.foca_mobile.model.User
 import com.example.foca_mobile.utils.LoginPrefs
@@ -21,10 +20,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var currentUser: User
-
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         //THIS IS WHERE WE DECIDE WHO IS LOGIN
         if (currentUser.role == "USER") {
             //USER FRAGMENT
-            val userHomeFragment = HomeFragment()
+            val userHomeFragment = UserHomeFragment()
             val messageFragment = ListMessageFragment()
 
             binding.bottomNavigation.setItemSelected(R.id.home)
@@ -69,16 +64,6 @@ class MainActivity : AppCompatActivity() {
                         .show()
                     R.id.user -> toLoginScreen();//sign out
                 }
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        val userhomeFragment = UserHomeFragment()
-        binding.bottomNavigation.setItemSelected(R.id.home)
-        binding.bottomNavigation.showBadge(R.id.message, 2)
-        setCurrentFragment(userhomeFragment)
-
-        binding.bottomNavigation.setOnItemSelectedListener{ id ->
-            when (id) {
-                R.id.home -> setCurrentFragment(userhomeFragment)
             }
         }
     }
@@ -97,5 +82,4 @@ class MainActivity : AppCompatActivity() {
         startActivity(it);
         this.finish();
     }
-
 }
