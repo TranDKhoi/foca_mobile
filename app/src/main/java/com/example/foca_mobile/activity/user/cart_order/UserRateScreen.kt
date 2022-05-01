2280bd6af1c5304c4aeab9383f1eafdcb3e24f2d
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2022-05-01 20:35:35 +0700

    fix rate screen

--- app/src/main/java/com/example/foca_mobile/activity/user/cart_order/UserRateScreen.kt
+++ app/src/main/java/com/example/foca_mobile/activity/user/cart_order/UserRateScreen.kt
@@ -1,7 +1,10 @@
 package com.example.foca_mobile.activity.user.cart_order
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import com.example.foca_mobile.databinding.ActivityUserRateScreenBinding
 
 class UserRateScreen : AppCompatActivity() {
@@ -18,5 +21,13 @@ class UserRateScreen : AppCompatActivity() {
             finish()
         }
 
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm = this!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(this!!.currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
