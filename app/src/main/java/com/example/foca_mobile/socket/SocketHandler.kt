595bc816483c563cd7bc7dd98709cb6f41986c20
Đỗ Đạt <ddatdt12@gmail.com>
2022-05-03 20:38:32 +0700

    change socket url server

--- app/src/main/java/com/example/foca_mobile/socket/SocketHandler.kt
+++ app/src/main/java/com/example/foca_mobile/socket/SocketHandler.kt
@@ -18,7 +18,7 @@ object SocketHandler {
             val options = IO.Options.builder()
                 .setAuth(singletonMap("token", token.toString()))
                 .build()
-            mSocket = IO.socket("http://10.0.2.2:5000", options)
+            mSocket = IO.socket("https://foca-backend.herokuapp.com", options)
             Log.d("mSocket.connect()", "RUNNING")
             mSocket.connect()
 
