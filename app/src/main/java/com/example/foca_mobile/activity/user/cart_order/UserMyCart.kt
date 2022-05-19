48bf1416b8257f730a90dd559b0685598ed1f7ba
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2022-05-20 01:59:56 +0700

    add language

--- app/src/main/java/com/example/foca_mobile/activity/user/cart_order/UserMyCart.kt
+++ app/src/main/java/com/example/foca_mobile/activity/user/cart_order/UserMyCart.kt
@@ -15,10 +15,6 @@ class UserMyCart : Fragment() {
     private lateinit var tabTitle: ArrayList<String>
     private lateinit var binding: FragmentUserMyCartBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
